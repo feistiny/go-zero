75f4b8a3db8e660f17ea8467cd289a21d4bce982
lzf <liuzhanfei166@126.com>
2025-12-13 14:22:49 +0800

    解决swagger多层引用的问题

--- tools/goctl/api/swagger/api.go
+++ tools/goctl/api/swagger/api.go
@@ -1,6 +1,11 @@
 package swagger
 
-import "github.com/zeromicro/go-zero/tools/goctl/api/spec"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/zeromicro/go-zero/tools/goctl/api/spec"
+)
 
 func fillAllStructs(api *spec.ApiSpec) {
 	var (
@@ -8,21 +13,27 @@ func fillAllStructs(api *spec.ApiSpec) {
 		structTypes = make(map[string]spec.DefineStruct)
 		groups      []spec.Group
 	)
+
+	// bytes2, _ := json.Marshal(api.Types)
+	// fmt.Printf("bytes2 %+v\n", string(bytes2))
 	for _, tp := range api.Types {
 		structTypes[tp.Name()] = tp.(spec.DefineStruct)
 	}
 
 	for _, tp := range api.Types {
-		filledTP := fillStruct("", tp, structTypes)
+		filledTP := fillStruct(tp, structTypes, "")
 		tps = append(tps, filledTP)
 		structTypes[filledTP.Name()] = filledTP.(spec.DefineStruct)
 	}
 
+	// bytes1, _ := json.Marshal(structTypes)
+	// fmt.Printf("bytes1 %+v\n", string(bytes1))
+
 	for _, group := range api.Service.Groups {
 		routes := make([]spec.Route, 0, len(group.Routes))
 		for _, route := range group.Routes {
-			route.RequestType = fillStruct("", route.RequestType, structTypes)
-			route.ResponseType = fillStruct("", route.ResponseType, structTypes)
+			route.RequestType = fillStruct(route.RequestType, structTypes, "")
+			route.ResponseType = fillStruct(route.ResponseType, structTypes, "")
 			routes = append(routes, route)
 		}
 		group.Routes = routes
@@ -32,44 +43,66 @@ func fillAllStructs(api *spec.ApiSpec) {
 	api.Types = tps
 }
 
-func fillStruct(parent string, tp spec.Type, allTypes map[string]spec.DefineStruct) spec.Type {
+func contains(list []string, s string) bool {
+	for _, v := range list {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+func CheckContains(parents []string, name string) {
+	if contains(parents, name) {
+		// 过滤掉空字符串
+		var filtered []string
+		for _, p := range parents {
+			if p != "" {
+				filtered = append(filtered, p)
+			}
+		}
+		panic(fmt.Sprintf("结构体存在递归引用 %+v %v", strings.Join(filtered, "->"), name))
+	}
+}
+
+func fillStruct(tp spec.Type, allTypes map[string]spec.DefineStruct, parents ...string) spec.Type {
 	switch val := tp.(type) {
 	case spec.DefineStruct:
 		var members []spec.Member
+		// 先把空结构的成员填充一下
+		if len(val.Members) == 0 {
+			st, ok := allTypes[val.RawName]
+			if ok {
+				val.Members = st.Members
+			}
+		}
+		// 再递归填充其子成员
 		for _, member := range val.Members {
 			switch memberType := member.Type.(type) {
 			case spec.PointerType:
 				member.Type = spec.PointerType{
 					RawName: memberType.RawName,
-					Type:    fillStruct(val.Name(), memberType.Type, allTypes),
+					Type:    fillStruct(memberType.Type, allTypes, append(parents, val.Name())...),
 				}
 			case spec.ArrayType:
 				member.Type = spec.ArrayType{
 					RawName: memberType.RawName,
-					Value:   fillStruct(val.Name(), memberType.Value, allTypes),
+					Value:   fillStruct(memberType.Value, allTypes, append(parents, val.Name())...),
 				}
 			case spec.MapType:
 				member.Type = spec.MapType{
 					RawName: memberType.RawName,
 					Key:     memberType.Key,
-					Value:   fillStruct(val.Name(), memberType.Value, allTypes),
+					Value:   fillStruct(memberType.Value, allTypes, append(parents, val.Name())...),
 				}
 			case spec.DefineStruct:
-				if parent != memberType.Name() { // avoid recursive struct
-					if st, ok := allTypes[memberType.Name()]; ok {
-						member.Type = fillStruct("", st, allTypes)
-					}
+				CheckContains(append(parents, val.Name()), memberType.Name())
+				if st, ok := allTypes[memberType.Name()]; ok {
+					member.Type = fillStruct(st, allTypes, append(parents, val.Name())...)
 				}
 			case spec.NestedStruct:
-				member.Type = fillStruct("", member.Type, allTypes)
+				member.Type = fillStruct(member.Type, allTypes, append(parents, val.Name())...)
 			}
 			members = append(members, member)
-		}
-		if len(members) == 0 {
-			st, ok := allTypes[val.RawName]
-			if ok {
-				members = st.Members
-			}
 		}
 		val.Members = members
 		return val
@@ -80,31 +113,26 @@ func fillStruct(parent string, tp spec.Type, allTypes map[string]spec.DefineStru
 			case spec.PointerType:
 				member.Type = spec.PointerType{
 					RawName: memberType.RawName,
-					Type:    fillStruct(val.Name(), memberType.Type, allTypes),
+					Type:    fillStruct(memberType.Type, allTypes, append(parents, val.Name())...),
 				}
 			case spec.ArrayType:
 				member.Type = spec.ArrayType{
 					RawName: memberType.RawName,
-					Value:   fillStruct(val.Name(), memberType.Value, allTypes),
+					Value:   fillStruct(memberType.Value, allTypes, append(parents, val.Name())...),
 				}
 			case spec.MapType:
 				member.Type = spec.MapType{
 					RawName: memberType.RawName,
 					Key:     memberType.Key,
-					Value:   fillStruct(val.Name(), memberType.Value, allTypes),
+					Value:   fillStruct(memberType.Value, allTypes, append(parents, val.Name())...),
 				}
 			case spec.DefineStruct:
-				if parent != memberType.Name() { // avoid recursive struct
-					if st, ok := allTypes[memberType.Name()]; ok {
-						member.Type = fillStruct("", st, allTypes)
-					}
+				CheckContains(append(parents, val.Name()), memberType.Name())
+				if st, ok := allTypes[memberType.Name()]; ok {
+					member.Type = fillStruct(st, allTypes, append(parents, val.Name())...)
 				}
 			case spec.NestedStruct:
-				if parent != memberType.Name() {
-					if st, ok := allTypes[memberType.Name()]; ok {
-						member.Type = fillStruct("", st, allTypes)
-					}
-				}
+				member.Type = fillStruct(member.Type, allTypes, append(parents, val.Name())...)
 			}
 			members = append(members, member)
 		}
@@ -119,18 +147,18 @@ func fillStruct(parent string, tp spec.Type, allTypes map[string]spec.DefineStru
 	case spec.PointerType:
 		return spec.PointerType{
 			RawName: val.RawName,
-			Type:    fillStruct(parent, val.Type, allTypes),
+			Type:    fillStruct(val.Type, allTypes, parents...),
 		}
 	case spec.ArrayType:
 		return spec.ArrayType{
 			RawName: val.RawName,
-			Value:   fillStruct(parent, val.Value, allTypes),
+			Value:   fillStruct(val.Value, allTypes, parents...),
 		}
 	case spec.MapType:
 		return spec.MapType{
 			RawName: val.RawName,
 			Key:     val.Key,
-			Value:   fillStruct(parent, val.Value, allTypes),
+			Value:   fillStruct(val.Value, allTypes, parents...),
 		}
 	default:
 		return tp
